402b35148b8b32b051aed72702581fc8c0ed0e52
Roozeppe Jean Pierre <roozeppe.jp@gmail.com>
2016-06-04 17:12:27 -0400

    prototype web site added

---
+++ Return Citizens/script.js
@@ -0,0 +1,8 @@
+//this.name;
+/*
+      <form onsubmit="myFunction()">
+      
+      <input type="text" id="uniqueID" name="firstname" style='width:80%;margin:-3px;border:2px inset #eee'><br>
+      </form>
+
+     */
